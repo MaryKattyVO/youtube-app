9d6fa83be6c978863c9666dbb57de0796017f612
MaryKattyVO <mkvicunaore@gmail.com>
2021-07-20 14:23:26 -0500

    consumiento el api de youtube

---
+++ src/app/services/youtube.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient, HttpParams } from '@angular/common/http'
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class YoutubeService {
+  private youtubeUrl    = 'https://www.googleapis.com/youtube/v3'
+  private apiKey        = 'AIzaSyB4ozvYx-RA8mBbQUbjDazzrXnBQ9rRrao'
+  private playlist      = 'UUuaPTYj15JSkETGnEseaFFg'
+  private nextPageToken = ''
+
+  constructor( private http: HttpClient) { 
+  }
+
+  getVideos() {
+    const url= `${this.youtubeUrl}/playlistItems`;
+    const params = new HttpParams()
+      .set('part', 'snippet')
+      .set('maxResults', '10')
+      .set('playlistId', this.playlist )
+      .set('key', this.apiKey)
+    ;
+    return this.http.get(url, {params});
+  }
+}
